78315f9e132382f7d3e31a8009c4391609345817
abhijit7756 <155934575+abhijit7756@users.noreply.github.com>
2024-01-08 09:09:28 +0530

    Create atm.py

---
+++ atm.py
@@ -0,0 +1,7 @@
+import sys
+import datetime
+
+def main():
+  print("hallo user welcome plase select the option")
+  print("prees 1, for withdrow prass 2, for diposit prees 3 for show balance.")
+  
